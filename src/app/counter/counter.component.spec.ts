748913f8871c78ec77085afbb1f325bb7d534136
juliusbacosa <juliusbacosa@gmail.com>
2018-01-16 10:51:30 +0800

    testing

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -24,9 +24,53 @@ describe('CounterComponent', () => {
      htmlElement = debugElement.nativeElement;
   });
 
+  it('should increment the counter number by one', () => {
+    // Arrange
+    const initialValue = component.counter;
+
+    // Act
+    component.increment();
+    fixture.detectChanges();
+    const newValue = component.counter;
+
+    // Assert
+    expect(newValue).toBeGreaterThan(initialValue);
+  });
+
+  it('should decrement the counter number by one', () => {
+    // Arrange
+    const initialValue = component.counter;
+
+    // Act
+    component.decrement();
+    fixture.detectChanges();
+    const newValue = component.counter;
+
+    // Assert
+    expect(newValue).toBeLessThan(initialValue);
+  });
+
+  it('should display the counter number on screen, after being incremented by one', () => {
+
+    component.increment();
+    fixture.detectChanges();
+
+    // Assert that the test on screen is of Number: 1
+    expect(htmlElement.textContent).toEqual('Number: 2');
+  });
+
+  it('should display the counter number on screen, after being decremented by one', () => {
+
+    component.decrement();
+    fixture.detectChanges();
+
+    // Assert that the test on screen is of Number: 1
+    expect(htmlElement.textContent).toEqual('Number: 0');
+  });
+
   it('should display the current number of the counter', () => {
     // Assert that the test on screenis of Number: 1
-    expect(htmlElement.textContent).toEqual('Number: 2');
+    expect(htmlElement.textContent).toEqual('Number: 1');
 
   });
 
